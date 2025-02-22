2edd4923e0ce91dd47e1aee32a9448c488afbc93
JLopez521 <jlopez521@csu.fullerton.edu>
2025-02-22 12:38:24 -0800

    Update main.cc

--- main.cc
+++ main.cc
@@ -6,3 +6,13 @@
 
 using namespace std;
 
+int main() {
+  string filename;
+  char mode;
+  cout << "Enter the file name: ";
+  getline(cin >> ws, filename);
+
+  cout << "1. Encrypt a file\n" << "2. Decrypt a file\n";
+  cin >> mode;
+
+  if (mode == '1' || mode == '2')
